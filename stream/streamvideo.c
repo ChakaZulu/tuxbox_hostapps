944d69d7ce92e1bf91145d1ced1ff901aa50d882
Hunz <Hunz>
2002-02-06 16:35:15 +0000

    fixes

--- stream/streamvideo.c
+++ stream/streamvideo.c
@@ -1,5 +1,5 @@
 // TCP Video/Audio - PES Streamer
-#include <iostream.h>
+//#include <iostream.h> /* wofür? */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,26 +20,27 @@
 #define DEST_PORT_AUDIO 40001
 #define DEST_PORT_VIDEO 40000
 
+#define BLOCKSIZE_AUDIO 1000
+#define BLOCKSIZE_VIDEO 1400
+
 int main(int argc, char *argv[])
 {
+  
+  char *aname=argv[2], *vname=argv[1];
+  int audiofd, videofd;
+  fd_set writefds;
+  int sockfda,sockfdv;
+  struct sockaddr_in dest_addra, dest_addrv;
+  
+  unsigned char audiobuf[BLOCKSIZE_AUDIO], videobuf[BLOCKSIZE_VIDEO];
+  int rbaudio;  //  Read Bytes Audio
+  int rbvideo;  //         and Video
+  int bytes_senta;
+  int bytes_sentv;
 
-char *aname=argv[2];
-char *vname=argv[1];
+  audiofd = open(aname, O_RDONLY, S_IRWXU);
+  videofd = open(vname, O_RDONLY, S_IRWXU); 
 
-int audiofd;
-int videofd;
-audiofd = open(aname, O_RDONLY, S_IRWXU);
-videofd = open(vname, O_RDONLY, S_IRWXU);
-
-char audiobuf[2500];
-char videobuf[2500];
-int rbaudio;  //  Received Bytes Audio
-int rbvideo;  //             and Video
-int bytes_senta;
-int bytes_sentv;
-
-        int sockfda;
-        struct sockaddr_in dest_addra;
         sockfda = socket(AF_INET, SOCK_STREAM, 0);
         dest_addra.sin_family = AF_INET;
         dest_addra.sin_port = htons(DEST_PORT_AUDIO);
@@ -48,8 +49,6 @@ int bytes_sentv;
 	fcntl(sockfda, F_SETFL, O_NONBLOCK );
         connect(sockfda, (struct sockaddr *)&dest_addra, sizeof(struct sockaddr));
 	
-	int sockfdv;
-        struct sockaddr_in dest_addrv;
         sockfdv = socket(AF_INET, SOCK_STREAM, 0);
         dest_addrv.sin_family = AF_INET;
         dest_addrv.sin_port = htons(DEST_PORT_VIDEO);
@@ -60,34 +59,45 @@ int bytes_sentv;
 
 
 //Create FD_SET's for select
-fd_set writefds;
 FD_ZERO(&writefds);
 FD_SET(sockfda,&writefds);
 FD_SET(sockfdv,&writefds);
 
 // 500ms muss der Audio vor Video kommen
-rbaudio=read(audiofd, audiobuf, 10);
+rbaudio=read(audiofd, audiobuf, BLOCKSIZE_AUDIO);
 bytes_senta = send(sockfda, audiobuf, rbaudio, 0);
-usleep(500);
-rbvideo=read(videofd, videobuf, 10);
+usleep(500000);
+
+/* shouldn't be necessary */
+/*
+rbvideo=read(videofd, videobuf, BLOCKSIZE_VIDEO);
 bytes_sentv = send(sockfdv, videobuf, rbvideo, 0); 
+*/
 
-
-while(1) 
-    if (select(7, 0, &writefds, 0 ,NULL)!=-1)
-{
-	rbaudio=read(audiofd, audiobuf, 10);
-	rbvideo=read(videofd, videobuf, 10);
-	bytes_sentv = send(sockfdv, videobuf, rbvideo, 0);
-	bytes_senta = send(sockfda, audiobuf, rbaudio, 0);
-}
-    else  {
-		if (FD_ISSET(sockfdv, &writefds)!=-1) { rbvideo=read(videofd, videobuf, 10); bytes_sentv = send(sockfdv, videobuf, rbvideo, 0); }
-                if (FD_ISSET(sockfda, &writefds)!=-1) { rbaudio=read(audiofd, audiobuf, 10); bytes_senta = send(sockfda, audiobuf, rbaudio, 0); }
-	}
-			
+ while(1) {
+   if (select(7, 0, &writefds, 0 ,NULL)>0)
+     {
+       if (FD_ISSET(sockfda, &writefds)) {
+	 rbaudio=read(audiofd, audiobuf, BLOCKSIZE_AUDIO);
+	 if(!(bytes_senta = send(sockfda, audiobuf, rbaudio, 0))) {
+	  printf("Audio: EOF reached\n");
+	  break;
+	 }
+       }
+       if (FD_ISSET(sockfdv, &writefds)) {
+	 rbvideo=read(videofd, videobuf, BLOCKSIZE_VIDEO);
+	 if(!(bytes_sentv = send(sockfdv, videobuf, rbvideo, 0))) {
+	   printf("Video: EOF reached\n");
+	   break;
+	 }
+       }
+     }
+   FD_SET(sockfda,&writefds);
+   FD_SET(sockfdv,&writefds);
+ }
+ close(videofd);
+ close(audiofd);
+ close(sockfdv);
+ close(sockfda);
 return 0;
 }
-
-
-	 
