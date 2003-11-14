911bf83daf8e0810a4da0e41fa96fc29c0670d5b
kerlimann <kerlimann>
2003-11-14 14:36:14 +0000

    sample sql data

---
+++ gui-tuxcal/data.sql
@@ -0,0 +1,16 @@
+# Dumping data for table `tuxcal`
+#
+
+INSERT INTO tuxcal VALUES ('z', '10.02.-21.02', 'Winterferien');
+INSERT INTO tuxcal VALUES ('z', '18.04.-25.04', 'Osterferien');
+INSERT INTO tuxcal VALUES ('z', '12.07.-27.08', 'Sommerferien');
+INSERT INTO tuxcal VALUES ('z', '20.10.-30.10', 'Herbstferien');
+INSERT INTO tuxcal VALUES ('z', '22.12.-03.01', 'Winterferien');
+INSERT INTO tuxcal VALUES ('g', '29.05.', 'test');
+INSERT INTO tuxcal VALUES ('t', '23.05.', 'Blumen schenken!!!');
+INSERT INTO tuxcal VALUES ('t', '12.05.2003', 'fgewfgjs');
+INSERT INTO tuxcal VALUES ('g', '13.05.', 'Geburtstag test');
+INSERT INTO tuxcal VALUES ('t', '20.04.', 'Ostereier suchen');
+INSERT INTO tuxcal VALUES ('g', '12.04.1964', 'Gans Heizer');
+INSERT INTO tuxcal VALUES ('t', '13.05.', 'Abtauchen');
+
