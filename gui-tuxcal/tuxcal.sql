6ffee56f2dd04606265095a82e1f62a29bc6eb6c
kerlimann <kerlimann>
2003-11-14 14:21:00 +0000

    update

---
+++ gui-tuxcal/tuxcal.sql
@@ -0,0 +1,9 @@
+#
+# Table structure for table `tuxcal`
+#
+
+CREATE TABLE tuxcal (
+  typ char(1) NOT NULL default '',
+    datum varchar(12) NOT NULL default '',
+      betreff varchar(20) NOT NULL default ''
+      ) TYPE=MyISAM;
