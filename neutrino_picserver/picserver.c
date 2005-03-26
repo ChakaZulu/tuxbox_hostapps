4d875beb4992922df4901188b390840c7f7c5a95
zwen <zwen>
2005-03-26 14:03:48 +0000

    - better 24 bit to 15 bit color reduction algorithm (floyd steinberg error diffusion)

--- neutrino_picserver/picserver.c
+++ neutrino_picserver/picserver.c
@@ -85,6 +85,7 @@ void jpeg_cb_error_exit(j_common_ptr cinfo)
 	(*cinfo->err->output_message) (cinfo);
 	longjmp(mptr->envbuffer,1);
 }
+
 unsigned char* jpeg_decode(char* filename, int* width, int* height, int wanted_width, int wanted_height)
 {
 	struct jpeg_decompress_struct cinfo;
@@ -127,6 +128,7 @@ unsigned char* jpeg_decode(char* filename, int* width, int* height, int wanted_w
 	jpeg_create_decompress(ciptr);
 	jpeg_stdio_src(ciptr,fh);
 	jpeg_read_header(ciptr,TRUE);
+
 /*	ciptr->dct_method=JDCT_IFAST;
 	ciptr->out_color_space=JCS_RGB;
 	ciptr->do_fancy_upsampling=FALSE;
@@ -144,6 +146,9 @@ unsigned char* jpeg_decode(char* filename, int* width, int* height, int wanted_w
 	else
 		ciptr->scale_denom=1;
 
+	ciptr->scale_num=1;
+//	ciptr->scale_denom=3;
+	
 	jpeg_start_decompress(ciptr);
 
 	px=ciptr->output_width; py=ciptr->output_height;
@@ -260,6 +265,126 @@ unsigned char * to555(unsigned char * orgin,int size)
 	free(orgin);
 	return np;
 }
+void c32_15(unsigned char r, unsigned char g , unsigned char b , unsigned char* d)
+{
+		*d     = ((r >> 1) & 0x7C) | (g >> 6);
+		*(d+1) = ((g << 2) & 0xE0) | (b >> 3);
+}
+
+/*
+	This is an implementation of the floyd steinberg error diffusion algorithm
+	adapted for 24bit to 15bit color reduction.
+
+	For a description of the base alorithm see e.g.: 
+	http://www.informatik.fh-muenchen.de/~schieder/graphik-01-02/slide0264.html
+	*/
+
+#define FS_CALC_ERROR(color, index) \
+				p1 = p2 = (ptr[index] + (this_line_error_##color[ix]>>4)); \
+				if(p1>255)p1=255; if(p1<0)p1=0; \
+				color = (p1 & 0xF8) | 0x4; \
+				error = p2 - color; \
+				this_line_error_##color[ix-1] += (error * 7); \
+				next_line_error_##color[ix+1] += (error * 3); \
+				next_line_error_##color[ix]   += (error * 5); \
+				next_line_error_##color[ix-1] += error;
+				
+unsigned char * to555_floyd_steinberg_err_diff(unsigned char * orgin,int x, int y)
+{
+	int odd_line=1;
+	int ix,iy, error, p1, p2;
+	unsigned char r,g,b;
+	unsigned char *ptr, *dst;
+	unsigned char* np = (unsigned char*)malloc(x*y*2);
+	int *this_line_error_r;
+	int *this_line_error_g;
+	int *this_line_error_b;
+	int *next_line_error_r;
+	int *next_line_error_g;
+	int *next_line_error_b;
+	int *save_error_r;
+	int *save_error_g;
+	int *save_error_b;
+	int *error1_r = (int*)malloc((x+2)*sizeof(int));
+	int *error1_g = (int*)malloc((x+2)*sizeof(int));
+	int *error1_b = (int*)malloc((x+2)*sizeof(int));
+	int *error2_r = (int*)malloc((x+2)*sizeof(int));
+	int *error2_g = (int*)malloc((x+2)*sizeof(int));
+	int *error2_b = (int*)malloc((x+2)*sizeof(int));
+	
+	lprintf("Start error diffusion\n");
+	
+	this_line_error_r = error1_r;
+	this_line_error_g = error1_g;
+	this_line_error_b = error1_b;
+	next_line_error_r = error2_r;
+	next_line_error_g = error2_g;
+	next_line_error_b = error2_b;
+	memset (this_line_error_r, 0 , (x+2) * sizeof(int));
+	memset (this_line_error_g, 0 , (x+2) * sizeof(int));
+	memset (this_line_error_b, 0 , (x+2) * sizeof(int));
+	memset (next_line_error_r, 0 , (x+2) * sizeof(int));
+	memset (next_line_error_g, 0 , (x+2) * sizeof(int));
+	memset (next_line_error_b, 0 , (x+2) * sizeof(int));
+	ptr = orgin;
+	dst = np;
+
+	for(iy=0 ; iy < y ; iy++)
+	{
+		save_error_r = this_line_error_r;
+		this_line_error_r = next_line_error_r;
+		next_line_error_r = save_error_r;
+		save_error_g = this_line_error_g;
+		this_line_error_g = next_line_error_g;
+		next_line_error_g = save_error_g;
+		save_error_b = this_line_error_b;
+		this_line_error_b = next_line_error_b;
+		next_line_error_b = save_error_b;
+		memset (next_line_error_r, 0 , (x+2) * sizeof(int));
+		memset (next_line_error_g, 0 , (x+2) * sizeof(int));
+		memset (next_line_error_b, 0 , (x+2) * sizeof(int));
+		
+		if(odd_line)
+		{
+			for(ix=1 ; ix <= x ; ix++)
+			{
+				FS_CALC_ERROR(r,0);
+				FS_CALC_ERROR(g,1);
+				FS_CALC_ERROR(b,2);
+				c32_15(r,g,b,dst);
+				ptr+=3;
+				dst+=2;
+			}
+			odd_line=0;
+		}
+		else
+		{
+			ptr+=(x-1)*3;
+			dst+=(x-1)*2;
+			for(ix=x ; ix >= 1 ; ix--)
+			{
+				FS_CALC_ERROR(r,0);
+				FS_CALC_ERROR(g,1);
+				FS_CALC_ERROR(b,2);
+				c32_15(r,g,b,dst);
+				ptr-=3;
+				dst-=2;
+			}
+			ptr+=x*3;
+			dst+=x*2;
+			odd_line=1;
+		}
+	}
+	free(orgin);
+	free(error1_r);
+	free(error1_g);
+	free(error1_b);
+	free(error2_r);
+	free(error2_g);
+	free(error2_b);
+	lprintf("End error diffusion\n");
+	return np;
+}
 /* do string mapping */
 void convert(char* str)
 {
@@ -331,7 +456,8 @@ void* ServerThread(void* socket)
 
 	/* convert RGB888 (24 bit) to RGB555 (16bit) */
 	lprintf("Start downsampling\n");
-	img=to555(img, nx * ny);
+//	img=to555(img, nx * ny);
+	img=to555_floyd_steinberg_err_diff(img, nx, ny);
 	lprintf("End downsampling\n");
 	lprintf("pic size\n",time(NULL), nx*ny*2);
 
