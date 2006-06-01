9eb6826c407dd2a01a70fa8bdbfebb796a684d15
carjay <carjay>
2006-06-01 17:32:25 +0000

    - fix

--- ggrab/cbuffer.h
+++ ggrab/cbuffer.h
@@ -15,7 +15,7 @@ class CBuffer {
 		CBUFPTR		SearchStreamId(CBUFPTR ptr, int len, unsigned char pattern, unsigned char mask, unsigned char * p_id=0);
 		int		GetByteCount(void);
 		int             CopyBuffer(CBUFPTR ptr, unsigned char * pBuf, int len=16);
-		int 		CBuffer::RemovePadding(CBUFPTR lptr, int len);
+		int 		RemovePadding(CBUFPTR lptr, int len);
 		
 
 	private:
